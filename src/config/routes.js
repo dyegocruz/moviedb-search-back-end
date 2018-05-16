0cad9d1dc82177741667759a2f40a02f95c3397d
Dyego Cruz <dyegocruz@gmail.com>
2018-05-16 17:13:42 -0300

    Add controllers on routes.

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,11 +3,16 @@ import config from './index';
 import middleware from '../middleware';
 import initializeDb from './database';
 import genre from '../controller/genreController';
+// import movie from '../controller/movieController';
+import discover from '../controller/discoverController';
+import search from '../controller/searchController';
 
 const router = express();
 
 initializeDb((db) => {
   router.use(middleware({ config, db }));
+  router.use('/discover', discover({ config, db }));
+  router.use('/search', search({ config, db }));
   router.use('/genres', genre({ config, db }));
 });
 
